00087a57ac10e968bc92ef9d5de0b1402748a344
Philip Bauer <bauer@starzel.de>
2012-07-25 16:43:30 +0200

    initial commit

---
+++ starzel/firstitem/firstitemfolder.py
@@ -0,0 +1,43 @@
+from Acquisition import aq_base, aq_inner
+from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.interfaces import ISiteRoot
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
+
+
+class FirstItemView(BrowserView):
+    """Custom view for the Folders forwarding to the first item inside
+    """
+    template = ViewPageTemplateFile('templates/folder_listing.pt')
+
+    def __call__(self):
+        context = self.context
+        portal_state = getMultiAdapter((context, self.request), name=u'plone_portal_state')
+        portal_catalog = getToolByName(context, 'portal_catalog')
+        currentpath = '/'.join(context.getPhysicalPath())
+        folder_contents = portal_catalog(path=dict(query=currentpath, depth=1), sort_on='getObjPositionInParent')
+        if folder_contents:
+            first_item = folder_contents[0]
+            self.request.response.redirect(first_item.getURL())
+        else:
+            return self.template()
+
+
+class FirstItemView_Helper(BrowserView):
+
+    def redirect_source(self):
+        """ url of the parent if context is the target of a 'firstitem_view'
+        """
+        obj = self.context
+        parent = obj.__parent__
+        context_state = getMultiAdapter(
+            (aq_inner(self.context), self.request), name=u'plone_context_state')
+        if context_state.is_default_page():
+            obj = parent
+            parent = parent.__parent__
+        if getattr(aq_base(parent), 'layout', None) == 'firstitem_view':
+            position = parent.getObjectPosition(obj.id)
+            if position == 0:
+                return parent.absolute_url()
+        return False
---
+++ starzel/firstitem/interfaces.py
@@ -0,0 +1,5 @@
+from zope.interface import Interface
+
+class IFirstItemLayer(Interface): 
+    """Browser layer for this particular product 
+    """ 
